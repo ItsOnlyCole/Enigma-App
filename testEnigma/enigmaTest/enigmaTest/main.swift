2c971fcff4dbd03de84500f7585061ad012adb40
user153545 <user153545@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-05-04 20:13:13 -0400

    Create Test Project
    Created a console based test project to get enigma logic working.

---
+++ testEnigma/enigmaTest/enigmaTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  enigmaTest
+//
+//  Created by user153545 on 5/4/19.
+//  Copyright © 2019 ItsOnlyCole. All rights reserved.
+//
+
+//import Foundation
+
+print("Hello, World!")
+
